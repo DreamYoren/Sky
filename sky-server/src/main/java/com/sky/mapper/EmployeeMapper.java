b90b714e918107b924500820dd0e1d4c2044c3fb
unknown <you@example.com>
2025-02-02 23:56:41 +0800

    123

---
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -0,0 +1,31 @@
+package com.sky.mapper;
+
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
+import com.sky.entity.Employee;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface EmployeeMapper {
+
+    /**
+     * 根据用户名查询员工
+     * @param username
+     * @return
+     */
+    @Select("select * from sky_take_out.employee where username = #{username}")
+    Employee getByUsername(String username);
+
+    @Insert("INSERT INTO sky_take_out.employee (name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user, status"+
+    ")VALUES"+
+    " (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)#(name),#(username),#(password),#(phone),#(sex),#(idNumber),#(createTime),#(updateTime),#(createTime),#(createUser),#(updateUser),#(status))")
+    void insert(Employee employee);
+
+    //分页查询的方法
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    //根据主键动态修改属性
+    void update(Employee employee);
+}
